47f3d15d7da8c850b62ff02a2d4723fbde20c5d8
Futa Hirakoba <korosuke613613@gmail.com>
2016-06-29 23:50:52 +0900

    Modify: exit(1) >> return 1

--- rcheck.c
+++ rcheck.c
@@ -15,11 +15,33 @@ int rcheck(char* str, int max, int min){
     }
     if(frag) {
         ER_NO_INT; // エラーを標準出力する。
-        exit(1);
+        return 1;
     }
     if(atoi(str) > max || atoi(str) < min){
         ER_REMITES_OVER(min, max); // エラーを標準出力する。
-        exit(1);
+        return 1;
     }
     return 0;
+}
+
+/**
+ * アラビア数字をローマ数字に変換する手続き
+ * rtrans_print
+ * 返り値 なし
+ * 引数 変換するアラビア数字、変換表
+ *_print
+ * 返り値 なし
+ * 引数 変換するアラビア数字、変換表
+ **/
+void rtrans_print(int num, Roman_arabic* a){
+    int i=0;
+    char str[BUFSIZE]="0";
+
+    while(num != 0){
+        if(num / a[i].arabic >= 1){
+            strcat(str, a[i].roman);
+            num -= a[i].arabic;
+        }else i++;
+    }
+    printf("%s\n",str+1);
 }
